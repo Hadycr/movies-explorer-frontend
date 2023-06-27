fe35a3858723e3559ef53374970ed8f2458611ce
Hadycr <hadycr@gmail.com>
2023-06-27 23:23:48 +0400

    рабочий вариант header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Header({isLogIn, email, userStatus, pathname, onClick }) {
+  return (
+    <header className="header">
+      <div className="header__logo"></div>
+      <div className='header__container'>
+        {isLogIn ? <div className='header__login'>
+                     <a href=''>Фильмы</a>
+                     <a href=''>Сохраненын Фильмы</a>
+                     <Link to={pathname} onClick = {onClick} className='header__button button'>{userStatus}</Link>
+                   </div> : <div classname="header__notlogin">
+                              <Link to={pathname} onClick = {onClick} className='header__button button'>{userStatus}</Link>
+                              <Link to={pathname} onClick = {onClick} className='header__button button'>{userStatus}</Link>
+                            </div>}
+      </div>
+    </header>
+  )
+}
+
+export default Header;
